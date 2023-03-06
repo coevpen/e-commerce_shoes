14dca5554fbc3ce668524f26884e30fa7e95b6c1
coevpen <koinuevins@gmail.com>
2023-03-06 16:55:34 -0600

    added ProdcutOrder model and updated associations

--- models/index.js
+++ models/index.js
@@ -4,8 +4,27 @@ const Brand = require(`./Brand`);
 const Category = require(`./Category`);
 const Customer = require(`./Customer`);
 const Order = require(`./Order`);
+const ProductOrder = require(`./ProductOrder`);
 
 // associations
 Customer.hasMany(Order, {
     foreignKey: 'customer_id'
 });
+
+Order.belongsTo(Customer,{
+    foreignKey: 'order_id'
+});
+
+Order.hasMany(Product,{
+    through: ProductOrder,
+    foreignKey: 'order_id'
+});
+
+Product.belongsToMany(Order, {
+    through: ProductOrder,
+    foreignKey: 'product_id'
+});
+
+Category
+
+Brand
