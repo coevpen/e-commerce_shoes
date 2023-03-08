45625f14b521816d4d1c6d2f422f05c5dcfeace6
Aidan Chang <aidan.t.chang@gmail.com>
2023-03-07 18:05:09 -0600

    Update index.js
    removed productOrder

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,13 +4,11 @@ const productRoutes = require('./product-routes');
 const brandRoutes = require('./brand-routes');
 const customerRoutes = require('./customer-routes');
 const orderRoutes = require('./order-routes');
-const productOrderRoutes = require('./productorder-routes');
 
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
 router.use('/brands', brandRoutes);
 router.use('/customers', customerRoutes);
 router.use('/orders', orderRoutes);
-router.use('/productorders', productOrderRoutes);
 
 module.exports = router;
