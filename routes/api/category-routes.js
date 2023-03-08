d431ee7eca3b8a72b67bb6f3d66aed19d3885b03
Aidan Chang <aidan.t.chang@gmail.com>
2023-03-07 18:12:31 -0600

    Update category-routes.js
    removed productOrder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Brands, Category, Customer, Order, Product, ProductOrder } = require('../../models');
+const { Brands, Category, Customer, Order, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
