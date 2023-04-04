01e50f0ce38420d80fa4d3f08b37420aa109b9a5
Edward Morales III <49822111+edm0ra@users.noreply.github.com>
2023-04-04 18:15:06 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,22 +7,4 @@ router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
 });
 
-// create a connection to the MySQL database
-const connection = mysql.createConnection({
-  host: 'database-1.ctnc6cjecppj.us-east-2.rds.amazonaws.com',
-  port: '3306',
-  user: 'admin',
-  password: 'password123',
-  database: 'shoes_db'
-});
-
-// connect to the MySQL database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to MySQL database with connection ID: ' + connection.threadId);
-});
-
 module.exports = router;
