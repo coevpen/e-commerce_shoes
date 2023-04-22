ec3d5bfbe72178391c9381314f518c14c3e1cfc5
Claudia Slater <claudiaslater2002@gmail.com>
2023-04-22 13:57:52 -0500

    mandatory cart features finished except for updating db after placing order. small formatting changes.

--- public/assets/javascript/item.js
+++ public/assets/javascript/item.js
@@ -1,5 +1,15 @@
-function fillItem(e, id){
-  console.log(e);
+function addToCart(){
+  let size = document.querySelector("select").value;
+  var idm = localStorage.getItem("id");
+  if(localStorage.getItem("cart") == "null"){
+    localStorage.setItem("cart",idm + "-" + size);
+  }
+  else{
+    var s = localStorage.getItem("cart") + "," + idm + "-" + size;
+    localStorage.setItem("cart",s);
+  }
+  location.reload();
+  //history.go(0);
 }
 
 async function fillItemPage(){
@@ -8,7 +18,6 @@ async function fillItemPage(){
   const response = await fetch('/api/products/' +idm)
   .then(response => response.json())
   .then(data => db_response = data);
-  console.log(db_response);
   document.title="Soul to Soul - "+db_response.product_name;
   document.getElementById("product-name-label").innerHTML = db_response.product_name;
   document.getElementById("product-price-label").innerHTML = "$"+db_response.price;
